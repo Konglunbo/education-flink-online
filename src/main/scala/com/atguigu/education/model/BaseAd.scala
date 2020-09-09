ff3d38785adce3618500879781e43469498a03bb
Fanxin <403184590@qq.com>
2020-09-10 07:48:46 +0800

    20200911

---
+++ src/main/scala/com/atguigu/education/model/BaseAd.scala
@@ -0,0 +1,7 @@
+package com.atguigu.education.model
+
+/**
+ * @author shkstart
+ * @create 2020-09-10 7:19
+ */
+case class BaseAd(adid: Int, adname: String, dn: String)
---
+++ src/main/scala/com/atguigu/education/model/GlobalConfig.scala
@@ -0,0 +1,20 @@
+package com.atguigu.education.model
+
+/**
+ * @author shkstart
+ * @create 2020-09-10 7:11
+ */
+object GlobalConfig {
+
+
+  val BOOTSTRAP_SERVERS = ""
+  val ACKS=""
+
+
+  val KUDU_MASTER = ""
+  val KUDU_TABLE_DWDBASEAD = ""
+  val KUDU_TABLE_DWDBASEWEBSITE = ""
+  val KUDU_TABLE_DWDVIPLEVEL = ""
+  val KUDU_TABLE_DWSMEMBER = ""
+
+}
